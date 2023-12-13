121b026bbb395ac865b55b625f2c15635ffefc2d
Ryan Buck <buckr56@yahoo.com>
2023-12-13 15:45:41 -0600

    Breakout Filename and dayOne function

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,15 +3,20 @@ package main;
 import dayOne.DayOne;
 
 public class Main {
+
+    private static final String DAY_ONE_FILE_NAME = "dayOneInput.txt";
+
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
+        runDayOne();
+    }
 
-        // DAY ONE
+    public static void runDayOne() {
         DayOne dayOne = new DayOne();
-        int result = dayOne.sumIntsFromFile("src/dayOne/dayOneInput.txt");
+        int result = dayOne.sumIntsFromFile(DAY_ONE_FILE_NAME);
         System.out.print("Day One Part One answer: ");
         System.out.println(result);
-        result = dayOne.sumIntsFromFilePtTwo("src/dayOne/dayOneInput.txt");
+        result = dayOne.sumIntsFromFilePtTwo(DAY_ONE_FILE_NAME);
         System.out.print("Day One Part Two answer: ");
         System.out.println(result);
     }
